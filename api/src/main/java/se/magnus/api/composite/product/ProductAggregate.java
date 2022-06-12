d0a11118b17ccfd0cc4fdf8b89f5edfa4523775c
Alexandre Oliveira <alexmdo@gmail.com>
2022-06-11 22:34:36 -0300

    Finished implementing ProductAggregate, ProductCompositeServiceApplication and ProductCompositeServiceImpl Added application.yml, test-em-all.bash Integration tests in ProductCompositeServiceApplicationTests

--- api/src/main/java/se/magnus/api/composite/product/ProductAggregate.java
+++ api/src/main/java/se/magnus/api/composite/product/ProductAggregate.java
@@ -9,15 +9,15 @@ public class ProductAggregate {
     private final int weight;
     private final List<RecommendationSummary> recommendations;
     private final List<ReviewSummary> reviews;
-    private final ServiceAddress serviceAddress;
+    private final ServiceAddresses serviceAddresses;
 
-    public ProductAggregate(int productId, String name, int weight, List<RecommendationSummary> recommendations, List<ReviewSummary> reviews, ServiceAddress serviceAddress) {
+    public ProductAggregate(int productId, String name, int weight, List<RecommendationSummary> recommendations, List<ReviewSummary> reviews, ServiceAddresses serviceAddresses) {
         this.productId = productId;
         this.name = name;
         this.weight = weight;
         this.recommendations = recommendations;
         this.reviews = reviews;
-        this.serviceAddress = serviceAddress;
+        this.serviceAddresses = serviceAddresses;
     }
 
     public int getProductId() {
@@ -40,7 +40,7 @@ public class ProductAggregate {
         return reviews;
     }
 
-    public ServiceAddress getServiceAddress() {
-        return serviceAddress;
+    public ServiceAddresses getServiceAddresses() {
+        return serviceAddresses;
     }
 }
--- microservices/product-composite-service/src/test/java/se/magnus/microservices/composite/product/ProductCompositeServiceApplicationTests.java
+++ microservices/product-composite-service/src/test/java/se/magnus/microservices/composite/product/ProductCompositeServiceApplicationTests.java
@@ -1,16 +1,94 @@
 package se.magnus.microservices.composite.product;
 
+import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.mockito.Mockito;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
 import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.test.web.reactive.server.WebTestClient;
+import se.magnus.api.core.product.Product;
+import se.magnus.api.core.recommendation.Recommendation;
+import se.magnus.api.core.review.Review;
+import se.magnus.microservices.composite.product.services.ProductCompositeIntegration;
+import se.magnus.util.exceptions.InvalidInputException;
+import se.magnus.util.exceptions.NotFoundException;
+
+import java.util.Collections;
+
+import static java.util.Collections.*;
+import static org.mockito.Mockito.*;
 
 @RunWith(SpringRunner.class)
-@SpringBootTest
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 public class ProductCompositeServiceApplicationTests {
+
+	private static final int PRODUCT_ID_OK = 1;
+	private static final int PRODUCT_ID_NOT_FOUND = 2;
+	private static final int PRODUCT_ID_INVALID = 3;
+
+	@Autowired
+	private WebTestClient client;
+
+	@MockBean
+	private ProductCompositeIntegration compositeIntegration;
+
+	@Before
+	public void setUp() {
+		when(compositeIntegration.getProduct(PRODUCT_ID_OK)).thenReturn(new Product(PRODUCT_ID_OK, "name", 1,  "mock-address"));
+		when(compositeIntegration.getRecommendations(PRODUCT_ID_OK)).thenReturn(singletonList(new Recommendation(PRODUCT_ID_OK, 1, "author", 1, "content", "mock address")));
+		when(compositeIntegration.getReviews(PRODUCT_ID_OK)).thenReturn(singletonList(new Review(PRODUCT_ID_OK, 1, "author", "subject", "content", "mock address")));
+
+		when(compositeIntegration.getProduct(PRODUCT_ID_NOT_FOUND)).thenThrow(new NotFoundException("NOT FOUND: " + PRODUCT_ID_NOT_FOUND));
+		when(compositeIntegration.getProduct(PRODUCT_ID_INVALID)).thenThrow(new InvalidInputException("INVALID: " + PRODUCT_ID_INVALID));
+	}
 
 	@Test
 	public void contextLoads() {
+	}
+
+	@Test
+	public void getProductById() {
+		client.get()
+				.uri("/product-composite/" + PRODUCT_ID_OK)
+				.accept(MediaType.APPLICATION_JSON)
+				.exchange()
+				.expectStatus().isOk()
+				.expectHeader().contentType(MediaType.APPLICATION_JSON)
+				.expectBody()
+					.jsonPath("$.productId").isEqualTo(PRODUCT_ID_OK)
+					.jsonPath("$.recommendations.length()").isEqualTo(1)
+					.jsonPath("$.reviews.length()").isEqualTo(1);
+	}
+
+	@Test
+	public void getProductNotFound() {
+		client.get()
+				.uri("/product-composite/" + PRODUCT_ID_NOT_FOUND)
+				.accept(MediaType.APPLICATION_JSON)
+				.exchange()
+				.expectStatus().isNotFound()
+				.expectHeader().contentType(MediaType.APPLICATION_JSON)
+				.expectBody()
+					.jsonPath("$.path").isEqualTo("/product-composite/" + PRODUCT_ID_NOT_FOUND)
+					.jsonPath("$.message").isEqualTo("NOT FOUND: " + PRODUCT_ID_NOT_FOUND);
+	}
+
+	@Test
+	public void getProductInvalidInput() {
+		client.get()
+				.uri("/product-composite/" + PRODUCT_ID_INVALID)
+				.accept(MediaType.APPLICATION_JSON)
+				.exchange()
+				.expectStatus().isEqualTo(HttpStatus.UNPROCESSABLE_ENTITY)
+				.expectHeader().contentType(MediaType.APPLICATION_JSON)
+				.expectBody()
+					.jsonPath("$.path").isEqualTo("/product-composite/" + PRODUCT_ID_INVALID)
+					.jsonPath("$.message").isEqualTo("INVALID: " + PRODUCT_ID_INVALID);
 	}
 
 }
